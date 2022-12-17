edbd18742cde56bd846cfb6014f0f859c028a4d0
Vladislav Vertiyov <v.vertiyov@yandex.ru>
2022-12-18 01:37:39 +0300

    Second try

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, mixins, filters
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Group, Post, User, Follow
+from posts.models import Group, Post
 from .permissions import AuthorOrReadOnly
 from .serializers import (
     CommentSerializer, GroupSerializer, PostSerializer, FollowSerializer
